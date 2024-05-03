4018454a27c03be895ed30e75c0fc03f80ec8518
Aayush <aayush.paigwar123@gmail.com>
2024-05-03 22:27:17 +0530

    android fix

--- lib/widgets/googlemaps.dart
+++ lib/widgets/googlemaps.dart
@@ -1,20 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
-  final double latitude;
-  final double longitude;
+// class MapScreen extends StatelessWidget {
+//   final double latitude;
+//   final double longitude;
 
-  const MapScreen({super.key, required this.latitude, required this.longitude});
+//   const MapScreen({super.key, required this.latitude, required this.longitude});
 
-  @override
-  Widget build(BuildContext context) {
-    return GoogleMap(
-      /*Not have the subscription for google maps integrations */
-      initialCameraPosition: CameraPosition(
-        target: LatLng(latitude, longitude),
-        zoom: 14.4746,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return GoogleMap(
+//       /*Not have the subscription for google maps integrations */
+//       initialCameraPosition: CameraPosition(
+//         target: LatLng(latitude, longitude),
+//         zoom: 14.4746,
+//       ),
+//     );
+//   }
+// }
